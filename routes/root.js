618ba72dec8e94729ed6aae36b44a95d2980c5b1
Alex <aegusev91@gmail.com>
2026-08-26 20:45:08 +0300

    add Eta

--- routes/root.js
+++ routes/root.js
@@ -1,6 +1,7 @@
 export default async function (app, options) {
   app.get('/', async function (req, res) {
-    res.send('Welcome to Hexlet!')
-    // return { root: true }
+    res.type('text/html');
+    console.log('header: ', res.getHeader('content-type'));
+    res.send('<h1>Hello Hexlet</h1>')
   })
 }
