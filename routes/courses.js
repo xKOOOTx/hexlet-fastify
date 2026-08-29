65ad0ba747dd55109717227696afa30b62bc26f0
Alex <aegusev91@gmail.com>
2026-08-30 00:54:12 +0300

    добавил форму поиска по курсам

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,31 @@
+import { courses } from '../mockdata/courses.js'
+
 export default (app) => {
     app.get('/courses', async (req, res) => {
-        return res.view('pages/courses')
+
+        const { name, description } = req.query
+
+        const filteredCourses = courses.filter(course => {
+            const matchesName = !name || 
+                course.name.toLowerCase().includes(name.toLowerCase())
+
+            const matchesDescription = !description ||
+                course.description.toLowerCase().includes(description.toLowerCase())
+
+            return matchesName && matchesDescription
+        })
+
+        return res.view('pages/courses/courses', {courses: filteredCourses, name, description})
+    })
+
+    app.get('/courses/:id', async (req, res) => {
+        const { id } = req.params
+        const course = courses.find(el => {
+            console.log('el: ', el)
+            return el.id === Number(id)
+        })
+
+        if(!course) return res.code(404).send('Курс с таким id не найден')
+        return res.view('pages/courses/course', course)
     })
 }
