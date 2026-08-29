91f82f5569cc64ae9d7580ed13eab67c22707f2b
Alex <aegusev91@gmail.com>
2026-08-29 17:01:26 +0300

    добавил пользователей

---
+++ mockdata/posts.js
@@ -0,0 +1,22 @@
+export const posts = [
+    {
+        id: 1,
+        description: 'Course 1'
+    },
+    {
+        id: 2,
+        description: 'Course 2'
+    },
+    {
+        id: 3,
+        description: 'Course 3'
+    },
+    {
+        id: 4,
+        description: 'Course 4'
+    },
+    {
+        id: 5,
+        description: 'Course 5'
+    },
+]
--- routes/root.js
+++ routes/root.js
@@ -1,29 +1,11 @@
+import { posts } from '../mockdata/posts.js'
+import { users } from '../mockdata/users.js'
 
-const posts = [
-    {
-        id: 1,
-        description: 'Course 1'
-    },
-    {
-        id: 2,
-        description: 'Course 2'
-    },
-    {
-        id: 3,
-        description: 'Course 3'
-    },
-    {
-        id: 4,
-        description: 'Course 4'
-    },
-    {
-        id: 5,
-        description: 'Course 5'
-    },
-]
 export default async (app, options) => {
   app.get('/', async (req, res) => res.view('index'))
+
   app.get('/about', async (req, res) => res.view('about'))
+
   app.get('/posts', async (req, res) => res.view('pages/posts', {posts}))
   app.get('/posts/:id', async(req, res) => {
     const { id } = req.params;
@@ -31,5 +13,14 @@ export default async (app, options) => {
 
     if (!post) return res.code(404).send('Такого поста нет');
     return res.view('pages/post', post)
+  })
+
+  app.get('/users', async (req, res) => res.view('pages/users/users', { users }))
+  app.get('/users/:id', async (req, res) => {
+    const { id } = req.params
+    const user = users.find(el => el.id === id)
+
+    if (!user) return res.code(404).send('Пользователь с таким id не найден')
+    return res.view('pages/users/user', user)
   })
 }
