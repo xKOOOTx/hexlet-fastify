718d5941af734dd09377407cc3b0a7fe7f34a406
Alex <aegusev91@gmail.com>
2026-08-30 13:14:30 +0300

    добавил добавление пользователя

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,14 @@ export default (app) => {
         if (!user) return res.code(404).send('Пользователь с таким id не найден')
         return res.view('pages/users/user', user)
     })
+    app.get('/users/new', async (req, res) => res.view('pages/users/user-new'))
+    app.post('/users/new', async (req, res) => {
+        const { username, email } = req.body
+        const normalizedEmail = email.trim().toLowerCase()
+        const foundUserByEmail = users.find(el => el.email === normalizedEmail)
+        if (foundUserByEmail) return res.code(409).view('pages/users/user-new', {username, email, message: 'Пользователь с таким email уже существует'})
+
+        users.push({id: `qwe-asd-${users.length}`, username, email})
+        return res.redirect('/users');
+    })
 }
