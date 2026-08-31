09d3c49e0ea23e0532455cc244394897fd024b5b
Alex <aegusev91@gmail.com>
2026-09-01 01:19:45 +0300

    посты

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,11 @@
 import { posts } from "../mockdata/posts.js"
 
 export default (app) => {
-    app.get('/posts', async (req, res) => {
+    app.get('/posts', { name: 'posts' }, async (req, res) => {
         return res.view('pages/posts/posts', {posts})
     })
 
-    app.get('/posts/:id', async(req, res) => {
+    app.get('/posts/:id', { name: 'post' }, async(req, res) => {
         const { id } = req.params;
         const post = posts.find(el => el.id === Number(id))
 
