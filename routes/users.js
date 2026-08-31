dfd6d21530503d33c1bb462c2b385d20ee68973b
Alex <aegusev91@gmail.com>
2026-09-01 01:22:09 +0300

    users -> u

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,17 @@ import { users } from '../mockdata/users.js'
 import yup from 'yup';
 
 export default (app) => {
-    app.get('/users', { name: 'users' }, async (req, res) => res.view('pages/users/users', { users }))
-    app.get('/users/:id', { name: 'user'}, async (req, res) => {
+    app.get('/u', { name: 'users' }, async (req, res) => res.view('pages/users/users', { users }))
+    app.get('/u/:id', { name: 'user'}, async (req, res) => {
         const { id } = req.params
         const user = users.find(el => el.id === id)
 
         if (!user) return res.code(404).send('Пользователь с таким id не найден')
         return res.view('pages/users/user', user)
     })
-    app.get('/users/new', { name: 'newUserForm' }, async (req, res) => res.view('pages/users/user-new'))
+    app.get('/u/new', { name: 'newUserForm' }, async (req, res) => res.view('pages/users/user-new'))
 
-    app.post('/users/new', {
+    app.post('/u/new', {
         name: 'createNewUser',
         attachValidation: true,
         schema: {
