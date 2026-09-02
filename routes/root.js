3aa5d13f241556677aadb9c4f51da80b56fff9ef
Alex <aegusev91@gmail.com>
2026-09-02 13:47:45 +0300

    допустим добавил аутентификацию

--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,5 @@
 import morgan from "morgan";
-
+import { users } from '../mockdata/users.js'
 
 export default async (app, options) => {
   /* root */
@@ -11,13 +11,31 @@ export default async (app, options) => {
         console.log('session:', req.session)
 
         const visited = req.cookies.visited
+        const userId = req.session.userId
+        const user = users.find(el => el.id === userId)
         const templateData = {
-            visited
+            visited,
+            userId,
+            user
         }
-        res.cookie("visited", true)
+        res.cookie('visited', true)
         return res.view('index', templateData)
     })    
     app.use(logger)
+
+    app.get("/increment", (req, res) => {
+        req.session.counter = req.session.counter || 0;
+        req.session.counter += 1;
+        res.send(req.session.counter)
+    });
+
+    app.post("/session", (req, res) => {
+        if (user.passwordDigest === encrypt(password)) {
+            req.session.userId = user.id;
+
+            console.log(req.session)
+        }
+    })
 
     /**
      * страницы about/posts/users и другие грузяться автоматически отдельными файлами в /routes about.js/courses.js и т.п. 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import { users } from '../mockdata/users.js'
 import yup from 'yup';
+import encrypt from '../utils/encrypt.js';
 
 export default (app) => {
     app.get('/u', { name: 'users' }, async (req, res) => res.view('pages/users/users', { users }))
@@ -66,5 +67,29 @@ export default (app) => {
         res.redirect(app.reverse('users'))
     })
 
+    app.get('/u/log-in', { name: 'log-in' }, (req, res) => {
+
+        res.view('pages/users/log-in')
+    })
+
+    app.post('/u/log-in', { name: 'user-log-in' }, (req, res) => {
+        const { email, password } = req.body
+
+        const user = users.find(el => el.email === email)
+
+        if (!user) return res.code(404).send('User not found')
+        req.session.userId = user.id
+
+        res.redirect('/')
+    })
+
+    app.get('/u/log-out', { name: 'log-out' }, (req, res) => {
+        if (!req.session.userId) {
+            res.redirect('/')
+        }
+
+        delete req.session.userId
+        res.redirect('/')
+    })
     
 }
